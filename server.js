2c622cd39b92d6733c5afd4d080c0b02d329a4db
Thiago Lanza <lanzathm@gmail.com>
2021-01-02 22:01:54 -0300

    Configurações de server criadas.

---
+++ server.js
@@ -0,0 +1,20 @@
+// Importa json server.
+const jsonServer = require('json-server');
+
+// Cria um novo server.
+const server = jsonServer.create();
+
+// Utiliza as chaves do json de db.json como endpoints
+const router = jsonServer.router('db.json');
+
+// Configuração padrão de middlewares.
+const middlewares = jsonServer.defaults();
+
+server.use('middlewares');
+server.use(router);
+
+const port = process.env.PORT || 8080;
+
+server.listen(port, () => {
+  console.log(`JSON server is running in ${port}`);
+});
